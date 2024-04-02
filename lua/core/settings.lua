2ba38585d8f8a0b0c41e094258552dfe7f691589
Lesly Kinzel <pc.leslykinzel@gmail.com>
2024-04-02 19:50:12 +1100

    Update comment / organize settings

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,10 +1,11 @@
 -- Editor Settings
 
 vim.opt.cursorline = true
+vim.opt.number = true
 
+-- Indentation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.opt.number = true
