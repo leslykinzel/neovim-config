321626a25c9a0566d179b50acdeb3499f0bafe4b
Lesly Kinzel <pc.leslykinzel@gmail.com>
2024-04-02 19:55:31 +1100

    Bootstrap Lazy.nvim, which-key

--- init.lua
+++ init.lua
@@ -2,3 +2,32 @@
 
 require("core.keymap")
 require("core.settings")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(
+{
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}
+)
