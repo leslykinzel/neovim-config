f7d0e69315a65527bc517bc24a548a75e080a611
Lesly Kinzel <pc.leslykinzel@gmail.com>
2024-06-16 22:58:32 +1000

    NvimTree adjust icon type.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,6 @@
 -- Editor Keymaps
-
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<C-j>', '10j', {})
 vim.keymap.set('n', '<C-k>', '10k', {})
+vim.keymap.set('n', '<leader>..', '<C-w>p', {})
--- lua/core/plugin_config/nvim_tree.lua
+++ lua/core/plugin_config/nvim_tree.lua
@@ -3,22 +3,26 @@ vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup{
     git = {
-        enable = true,
+        enable = true
     },
     renderer = {
+        indent_markers = {
+            enable = true
+        },
         highlight_git = true,
         icons = {
             show = {
                 git = true,
+                folder_arrow = false
             },
-        },
+        }
     },
     view = {
         side = "right",
     },
     filters = {
         custom = { "^.git$" }
-    },
+    }
 }
 
-vim.keymap.set('n', 'tr', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', 'tr', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
