0ddf4cd75b7dca58dc9a71b2e7b958f1ce144503
Lesly Kinzel <pc.leslykinzel@gmail.com>
2024-06-16 21:16:28 +1000

    Make lualine more explicit.

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -7,8 +7,45 @@ require('lualine').setup {
         lualine_a = {
             {
                 'filename',
-                path = 1,
+                path = 4,
             }
+        },
+        lualine_b = {
+            'branch'
+        },
+        lualine_c = {
+
+        },
+        lualine_x = {
+
+        },
+        lualine_y = {
+
+        },
+        lualine_z = {
+
+        }
+    },
+    inactive_sections = {
+        lualine_a = {
+            {
+                'filename',
+            }
+        },
+        lualine_b = {
+
+        },
+        lualine_c = {
+
+        },
+        lualine_x = {
+
+        },
+        lualine_y = {
+
+        },
+        lualine_z = {
+
         }
     }
 }
