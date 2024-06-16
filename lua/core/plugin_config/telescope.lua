f6302bcc712cb1cbdb30db37c1ff64159d8d118c
Lesly Kinzel <pc.leslykinzel@gmail.com>
2024-06-16 23:26:37 +1000

    Silence telescope

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ts', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ts', builtin.find_files, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gr', builtin.live_grep, { noremap = true, silent = true })
