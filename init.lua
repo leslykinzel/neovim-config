8900abf8c7802c921364489d8ae515f2bb6dba55
Lesly Kinzel <127464833+LeslyKinzel@users.noreply.github.com>
2024-06-19 19:25:26 +1000

    Remove comments

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
--- Blackout-Nvim
--- This file simply pulls together the various plugins and settings
--- defined within the core/ directory. If something is not being
--- loaded properly, you might need to :source this file in neovim.
 require("core.keymaps")
 require("core.settings")
 require("core.plugins")
