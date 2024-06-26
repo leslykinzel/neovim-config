b8dd9e84b096332459ef960646dfb858c117a45a
Lesly Kinzel <pc.leslykinzel@gmail.com>
2024-06-26 12:29:15 +1000

    Include gopls

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "clangd", "pyright" }
+    ensure_installed = { "lua_ls", "clangd", "pyright", "gopls" }
 })
 
 local on_attach = function(_, _)
@@ -21,5 +21,9 @@ require("lspconfig").clangd.setup {
 }
 
 require("lspconfig").pyright.setup {
+    on_attach = on_attach
+}
+
+require("lspconfig").gopls.setup {
     on_attach = on_attach
 }
