bb6f4b3b4393445ea575ef669c766c737d2aa748
Lesly Kinzel <pc.leslykinzel@gmail.com>
2024-10-02 14:55:46 +1000

    Documenting my settings.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,12 @@
 -- Editor Keymaps
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- For scrolling 10 lines at a time while holding Ctrl.
 vim.keymap.set('n', '<C-j>', '10j', {})
 vim.keymap.set('n', '<C-k>', '10k', {})
+
+-- This is supposed to mimic `cd ..` in my head .. means
+-- "go back", so <leader>.. focuses your cursor on the 
+-- previous pane.
 vim.keymap.set('n', '<leader>..', '<C-w>p', {})
