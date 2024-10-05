f35fea8d83349a60fec097fbeca48230bafaf20c
Lesly Kinzel <lesly.helios@proton.me>
2024-10-05 14:10:59 +1000

    Set lualine theme to 'auto'

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
     options = {
         icons_enabled = true,
-        theme = 'gruber-darker',
+        theme = 'auto',
         component_separators = '',
         section_separators = { left = '', right = ''},
     },
