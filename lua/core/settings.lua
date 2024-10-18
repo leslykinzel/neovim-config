5c52cfde26f8e62a1c5eaf7fb5383a73c81c3e6d
Lesly Kinzel <lesly.helios@proton.me>
2024-10-18 22:02:41 +1100

    Reorganise editor settings

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -2,6 +2,7 @@
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 4
+vim.opt.scroll = 10
 vim.opt.cmdheight = 0
 
 -- Indentation
@@ -9,8 +10,4 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
-
--- Other settings
-vim.opt.scroll = 10
-vim.opt.numberwidth = 2
 
