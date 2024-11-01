d9c8e5cabcff1c4d90e4bcf48bf192de8d0f1362
Lesly Kinzel <lesly.helios@proton.me>
2024-11-01 17:51:57 +1100

    Rebind telescope

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ts', builtin.find_files, { noremap = true, silent = true})
+vim.keymap.set('n', '<leader>tt', builtin.find_files, { noremap = true, silent = true})
 vim.keymap.set('n', '<leader>gr', builtin.live_grep, { noremap = true, silent = true })
