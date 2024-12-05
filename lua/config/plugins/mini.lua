4dd61fccc7443dd956155ba53b365b1058bbbf54
leslykinzel <lesly.helios@proton.me>
2024-12-06 00:27:43 +1100

    Configure Lazy, Statusline, Oil, Theme

---
+++ lua/config/plugins/mini.lua
@@ -0,0 +1,14 @@
+
+return {
+    {
+	"echasnovski/mini.nvim",
+	enabled = true,
+	config = function()
+	    local statusline = require "mini.statusline"
+	    statusline.setup { use_icons = true }
+
+	    local autopairs = require "mini.pairs"
+	    autopairs.setup()
+	end
+    },
+}
---
+++ lua/config/plugins/theme.lua
@@ -0,0 +1,11 @@
+
+return {
+    {
+    	"rebelot/kanagawa.nvim", 
+	enabled = true,
+	config = function() 
+	    vim.cmd.colorscheme "kanagawa-dragon"
+        end 
+    }
+}
+
