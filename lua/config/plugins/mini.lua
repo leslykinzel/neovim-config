cc4c261b5a40ee595c8b6232099bcb4951337a40
leslykinzel <lesly.helios@proton.me>
2025-01-17 13:09:59 +1100

    Set theme: Gruber Darker, Remove colours on statusline.

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -3,12 +3,25 @@ return {
 		"echasnovski/mini.nvim",
 		enabled = true,
 		config = function()
+			-- STATUSLINE
 			local statusline = require "mini.statusline"
 			statusline.setup { use_icons = true }
 
+			local modes = { "Normal", "Insert", "Visual", "Command", "Replace" }
+
+			for _, mode in ipairs(modes) do
+				vim.api.nvim_set_hl(0, "MiniStatuslineMode" .. mode, {
+					fg = "#FFFFFF",
+					bg = "#323232",
+					bold = false,
+				})
+			end
+
+			-- AUTOPAIRS
 			local autopairs = require "mini.pairs"
 			autopairs.setup()
 
+			-- KEYBIND HINTS
 			local keyhints = require "mini.clue"
 			keyhints.setup {
 				triggers = {
--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,47 +1,9 @@
 return {
-	"rebelot/kanagawa.nvim",
-	enabled = true,
-	config = function()
-		require("kanagawa").setup({
-			compile = false,             -- enable compiling the colorscheme
-			undercurl = true,            -- enable undercurls
-			commentStyle = { italic = true },
-			functionStyle = { bold = true },
-			keywordStyle = { italic = true},
-			statementStyle = {},
-			typeStyle = {},
-			transparent = false,         -- do not set background color
-			dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-			colors = {                   -- add/modify theme and palette colors
-				palette = {},
-				theme = {
-					wave = {},
-					lotus = {},
-					dragon = {
-						ui = {
-							float = {
-							},
-						}
-					},
-					all = {
-						ui = {
-							bg_gutter = "none"
-						}
-					}
-				},
-			},
-			overrides = function(colors)
-				local theme = colors.theme
-				return {}
-			end,
-			theme = "wave",              -- Load "wave" theme when 'background' option is not set
-			background = {               -- map the value of 'background' option to a theme
-				dark = "dragon",           -- try "dragon" !
-				light = "lotus"
-			},
-		})
-		vim.cmd.colorscheme "kanagawa"
-	end
-}
+	"blazkowolf/gruber-darker.nvim",
+	lazy = false,
+	priority = 1000,
 
+	config = function()
+		vim.cmd.colorscheme("gruber-darker")
+	end,
+}
