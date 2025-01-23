8ee14695cfbae012b4420bf6a68a066a52e12889
Lesly Kinzel <lesly.helios@proton.me>
2025-01-24 03:11:48 +1100

    Bind :noh

--- init.lua
+++ init.lua
@@ -10,11 +10,14 @@ vim.opt.scrolloff = 4
 vim.opt.cmdheight = 1
 vim.opt.tabstop = 4
 
--- Terminal Emulator
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
-
 -- Lazy
-vim.keymap.set("n", "<leader>L", ":Lazy<CR>", { silent = true })
+vim.keymap.set("n", "<leader>L", "<CMD>Lazy<CR>", { silent = true })
+
+-- Turn off search higlights
+vim.keymap.set("n", "<leader>hl", "<CMD>noh<CR>")
+
+-- Exit Terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- Oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
@@ -24,9 +27,4 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Search files from cwd" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep from cwd" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Search open buffers" })
-
--- Inactive indent line colour
-vim.cmd.highlight('IndentLine guifg=#282727')
--- Current indent line colour
-vim.cmd.highlight('IndentLineCurrent guifg=#393836')
 
