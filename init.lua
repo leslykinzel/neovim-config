5949cce17f68180516a0e8dffeddf5cd2d130c73
Lesly Kinzel <lesly.helios@proton.me>
2025-07-07 23:58:07 +1000

    Fix git url

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ local mini_path = path_package .. "pack/deps/start/mini.nvim"
 if not vim.loop.fs_stat(mini_path) then
   local clone_cmd = { 
     "git", "clone", "--filter=blob:none",
-    "https://github.com/echasnovski/mini.nvim", mini_path
+    "git@github.com/echasnovski/mini.nvim", mini_path
   }
   vim.fn.system(clone_cmd)
   vim.cmd("packadd mini.nvim | helptags ALL")
